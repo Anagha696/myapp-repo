4c15eaac89f543c5300bb6f0d8d9577ddce8df07
Anagha696 <64737400+Anagha696@users.noreply.github.com>
2025-03-27 12:16:56 +0530

    Update purge_queue.sh

--- purge_queue.sh
+++ purge_queue.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Set Azure Service Bus namespace and queue details
-NAMESPACE="queuenamespace"
-QUEUE_NAME="myapp-message-queue"
+QUEUE_NAME="newqueue"
+STORAGE_ACCOUNT="myuniquequeueaccount"
 RESOURCE_GROUP="QueueCleanupRG"
+STORAGE_ACCOUNT_KEY="cCNa9zuXR0kNMENFETHwODCtKRN5WLB1M4OiceEz+LcZ3QelGrYbBSoCINFkAQ9FeKPjwoR0GYZ7+AStBQoTSg=="
 SUBSCRIPTION_ID="71f6e2ba-7b47-47e6-801f-f1ef3570"
 
 # Login to Azure
@@ -13,7 +13,9 @@ az login
 az account set --subscription $SUBSCRIPTION_ID
 
 # Purge messages from the queue
-az servicebus queue purge --resource-group $RESOURCE_GROUP --namespace-name $NAMESPACE --queue-name $QUEUE_NAME --all-messages true
+
+az storage message clear --queue-name $QUEUE_NAME --account-name $STORAGE_ACCOUNT --account-key $STORAGE_ACCOUNT_KEY
+
 
 echo "Queue $QUEUE_NAME purged successfully."
 
